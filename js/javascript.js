2556c09f212ba88fee0fa6b441c07a8b445afff5
Arseniinub <censoled11111@gmail.com>
2024-11-30 15:44:26 +0300

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+<script>
+    // Используем jQuery для выполнения скрипта после загрузки страницы
+    $(document).ready(function() {
+        // Инициализируем CircleType.js и применяем к элементу #curved-text
+        new CircleType(document.getElementById('curved-text'))
+  
+    });
+</script>
